70b48fb462aa6af3748d7520ca891f0b445afee7
Tamadutta <tamadutta125@gmail.com>
2021-02-07 23:12:21 +0600

    first commit

---
+++ Assignment.js
@@ -0,0 +1,45 @@
+
+function kilometerToMeter(distance) {
+    var output = distance * 1000;
+    return output;
+}
+
+function budgetCalculator(watch, mobile, laptop) {
+    var output = watch * 50 + mobile * 100 + laptop * 500;
+    return output;
+}
+
+function hotelCost(day) {
+    if(day <= 10){
+        var rent = day * 100;
+        
+    }
+    else if(day <= 20){
+        var first10DaysRent = 1000;
+        var remainingDays = day - 10;
+        var rent = first10DaysRent + remainingDays * 80 ;
+       
+    }
+    else{
+        var first10DaysRent = 1000;
+        var second10DaysRent = 800;
+        var daysRemained = day - 20;
+        var thirdRemainingDaysRent = daysRemained * 50;
+        var rent = first10DaysRent + second10DaysRent + thirdRemainingDaysRent ;
+        
+    }
+    return rent;
+}
+
+function megaFriend(string) {
+    var longestName = "";
+    for (let i = 0; i < string.length; i++) {
+       if (string[i].length > longestName.length)
+           longestName = string[i];
+       
+    
+    }
+    return longestName;
+}
+var friendsName = megaFriend(["Rishika", "Hiya", "Niharika", "Imu", "rakha"]);
+console.log(friendsName);
